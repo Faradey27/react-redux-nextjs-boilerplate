f9d79caf9fdc3b417f7549f954bef18578c16451
Faradey27 <andrii.tiertyshnyi@gmail.com>
2017-09-03 22:04:18 +0300

    Moved project to src, updated package.json according to this

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const next = require('next');
 const compression = require('compression');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const app = next({ dir: __dirname, dev });
 const handle = app.getRequestHandler();
 
 const DEFAULT_PORT = 3000;
